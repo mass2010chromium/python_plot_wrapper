02331b1a6deed4eb734a95bb03219603a64a9543
llm_server <llm_server>
2025-06-13 16:48:45 -0400

    Update to Numpy 2.0

--- src/plot_wrapper/_brine_array_patch.py
+++ src/plot_wrapper/_brine_array_patch.py
@@ -8,7 +8,28 @@ except ImportError:
     from ._brine_patch import register
 import numpy as np
 
-NP_TYPES = list(itertools.chain(v for v in np.sctypes.values()))
+# Annoyed that numpy removed a nice way of enumerating this
+NP_INTEGER = [
+    np.bool_,
+    np.int8,
+    np.int16,
+    np.int32,
+    np.int64,
+    np.uint8,
+    np.uint16,
+    np.uint32,
+    np.uint64
+]
+NP_FLOAT = [
+    np.float16,
+    np.float32,
+    np.float64,
+]
+NP_COMPLEX = [
+    np.complex64,
+    np.complex128,
+]
+NP_TYPES = NP_INTEGER + NP_FLOAT + NP_COMPLEX
 
 @register(brine._custom_dumpable)
 def _dumpable_numpy(obj):
@@ -33,18 +54,16 @@ def _dump_array(obj, stream):
     brine._dump_str(str(obj.dtype), stream)
     brine._dump_bytes(obj.tobytes(), stream)
 
-
-
 @register(brine._custom_dumpers)
 def _dump_numpy(obj, stream):
     ret = True
     if type(obj) == np.ndarray:
         _dump_array(obj, stream)
-    elif type(obj) in np.sctypes['int'] or type(obj) in np.sctypes['uint']:
+    elif type(obj) in NP_INTEGER:
         brine._dump_int(obj, stream)
-    elif type(obj) in np.sctypes['float']:
+    elif type(obj) in NP_FLOAT:
         brine._dump_float(obj, stream)
-    elif type(obj) in np.sctypes['complex']:
+    elif type(obj) in NP_COMPLEX:
         brine._dump_complex(obj, stream)
     else:
         ret = False
