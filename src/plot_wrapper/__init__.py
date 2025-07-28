0480c6f54318d94800bbd3d522325c022f548f28
llm_server <llm_server>
2025-07-28 15:04:37 -0400

    Make open3d import failure a soft error

--- src/plot_wrapper/__init__.py
+++ src/plot_wrapper/__init__.py
@@ -1,2 +1,9 @@
-from ._matplotlib import MatplotlibWrapper, InteractiveMatplotlibWrapper
-from ._o3d import O3dVisWrapper
+try:
+    from ._matplotlib import MatplotlibWrapper, InteractiveMatplotlibWrapper
+except Exception as e:
+    print("plot_wrapper: Could not import matplotlib wrapper, maybe it is not installed?")
+
+try:
+    from ._o3d import O3dVisWrapper
+except Exception as e:
+    print("plot_wrapper: Could not import open3d wrapper, maybe it is not installed?")
